cddc80ff4412b014dc828a4ceaf8085ebd42b2e6
malinhodev <mcterasgo@gmail.com>
2023-10-26 10:08:14 -0300

    lógica de gerar os numeros finalizada

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const numbers = document.querySelectorAll(".number")
 const generateBtn = document.querySelector("#generate")
 
+function generateNumbers(){
+    const max = 25
+    const min = 1
+    const result = []
+
+    while (result.length < 15){
+        const number = Math.floor(Math.random() * (max - min + 1)) + min
+
+        if(!result.includes(number)){
+            result.push(number)
+        }
+    }
+
+    for(let i = 0; i < numbers.length; i++){
+        numbers[i].textContent = result[i]
+    }
+}
+
+generateBtn.addEventListener('click', generateNumbers)
